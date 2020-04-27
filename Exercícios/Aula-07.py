846fb3728f20ffc120891b3c480afecee367dbe9
Ryanfr191 <Ryanfr191@gmail.com>
2020-04-27 10:17:02 -0300

    Aula 07
    Operadores Aritiméticos

---
+++ Exercícios/Aula-07.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite um valor:'))
+n2 = int(input('Digite outro valor:'))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+p = n1 ** n2
+res = n1 % n2
+print('A soma vale {},\nO produto é {} \nE a divisão é {}'.format(s, m, d), end='.' )
+print('A divisão inteira é {} e a potência é {}'.format(di, p))
+
